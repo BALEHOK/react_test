bf72141e146ec8291754136789611477a9184944
BALEHOK <zhmurik@yandex.ru>
2018-03-16 12:38:03 +0200

    page layout

---
+++ app/src/components/articleTile.jsx
@@ -0,0 +1,22 @@
+
+import React, { Component } from 'react';
+import './articleTile.css';
+
+class articleList extends Component {
+  render() {
+    return (
+      <div className="article-tile">
+        <div className="article-tile-image">
+          <img src="http://www.stfc.ac.uk/stfc/cache/file/4821074D-0D31-4964-9318CA0AE81E0639.jpg" alt="CERN experiment discovers five new particles" />
+        </div>
+        <div className="article-tile-info">
+          <h3>CERN experiment discovers five new particles</h3>
+          <p>A new group of particles which have long been hiding in plain sight have finally been discovered thanks to the incredibly sensitive LHCb experiment at CERN. LHCb, otherwise known as the Large Hadron Collider beauty experiment, is one of seven experiments collecting data at the Large Hadron Collider at CERN. UK participation in the experiment is funded by STFC.</p>
+          <span>10 comments</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default articleList;
